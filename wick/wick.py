3b4360db22b8794a7e277330b2d9b4d6c754ad9d
Mark Wilson <secretchinamark@gmail.com>
2012-06-21 17:18:43 -0700

    wick: get tab urls, start to support screen names

--- wick/wick.py
+++ wick/wick.py
@@ -1,18 +1,52 @@
+import json
+
 from flask import Flask, request
 
 import applescripts
+import browser
 
 app = Flask(__name__)
 
-@app.route('/restart', methods=['POST'])
-def restart():
+@app.route('/enumerate', methods=['POST'])
+def enumerate():
+	return 'ok'
+
+@app.route('/<screen>/tabs', methods=['GET'])
+def tabs(screen):
+	return json.dumps(browser.get_tab_urls(screen))
+
+@app.route('/<screen>/restart', methods=['POST'])
+def restart(screen):
 	applescripts.run_script(applescripts.RESTART_CHROME)
 	return 'ok'
 
-@app.route('/new_tab', methods=['POST'])
-def new_tab():
-	applescripts.run_script(applescripts.NEW_TAB % {'url': request.form['url']})
+@app.route('/<screen>/show', methods=['POST'])
+def show(screen):
+	# will be a tab index or url
+	to_show = request.form['to_show']
+	print to_show
+	for index, url in browser.get_tab_urls(screen).iteritems():
+		if to_show in url:
+			#TODO activate tab #index
+			break
+	browser.new_tab(screen, to_show)
+	return 'ok'
+
+@app.route('/<screen>/reload', methods=['POST'])
+def reload(screen):
+	return 'ok'
+
+@app.route('/<screen>/next_tab', methods=['POST'])
+def next_tab(screen):
+	return 'ok'
+
+@app.route('/<screen>/prev_tab', methods=['POST'])
+def prev_tab(screen):
+	return 'ok'
+
+@app.route('/<screen>/close_tab', methods=['POST'])
+def close_tab(screen):
 	return 'ok'
 
 if __name__ == "__main__":
-	app.run()
+	app.run(host='0.0.0.0')
