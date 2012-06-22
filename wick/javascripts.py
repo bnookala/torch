9d934d2141469db52d75f9b49c9196ac745a59d4
Dmitriy Yakovlev <dmitriy@yelp.com>
2012-06-21 22:17:33 -0700

    centered enum

--- wick/javascripts.py
+++ wick/javascripts.py
@@ -3,7 +3,7 @@ SHOW_BIG_TEXT = """
 	var body = document.getElementsByTagName('body')[0];
 	var flameIDNode = document.createElement('h1');
 	var textNode = document.createTextNode('%(text)s');
-	flameIDNode.setAttribute('style', 'z-index:9999999;position:absolute;width:100%%;height:100%%;top:0;left:0;margin-left:auto;margin-right:auto;color:red;font-size:300px;');
+    flameIDNode.setAttribute('style', 'z-index:9999999;height:100%;text-align:center;margin: 0 auto;color:red;font-size:300px;');
 	flameIDNode.setAttribute('id', 'flame-enumerate-text');
 	flameIDNode.appendChild(textNode);
 	body.appendChild(flameIDNode);
